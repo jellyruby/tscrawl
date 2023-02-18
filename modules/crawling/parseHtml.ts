db666ac657d762873a4de40000b33533a4ea7251
jellyruby <50604410+jellyruby@users.noreply.github.com>
2023-02-18 23:54:49 +0900

    Html파싱하는 내용 추가

--- modules/crawling/parseHtml.ts
+++ modules/crawling/parseHtml.ts
@@ -1,1 +1,20 @@
-import cheerio from 'cheerio';
+import * as cheerio from 'cheerio';
+
+import TyphoonInfo from '../../type/typhoonInfo';
+
+//Html string을 파싱한다.
+export const parseHtml = (html:string) => {
+
+    const $ = cheerio.load(html);
+
+
+}
+
+//easterlywave에서 열대성 저기압 리스트를 가져온다.
+const parseTyphoonListFromEasterlyWave = ($:cheerio.CheerioAPI) => {
+
+    const lists = $(".level");
+    lists.each((index, list) => {
+        //crawling from easterlywave 
+    });
+}
