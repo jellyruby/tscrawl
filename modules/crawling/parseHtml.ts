35a11c2b32a5af895ac818c59f07e8e658741b56
jellyruby <50604410+jellyruby@users.noreply.github.com>
2023-02-19 22:31:39 +0900

    인터페이스 넣어서 추가

--- modules/crawling/parseHtml.ts
+++ modules/crawling/parseHtml.ts
@@ -2,19 +2,35 @@ import * as cheerio from 'cheerio';
 
 import TyphoonInfo from '../../type/typhoonInfo';
 
+interface parsetHtmlDetailInterface {
+    ($ : cheerio.CheerioAPI) : Array<TyphoonInfo>;
+}
+
+
 //Html string을 파싱한다.
-export const parseHtml = (html:string) => {
+export const parseHtml = (html:string, callBack:Function) => {
 
     const $ = cheerio.load(html);
-
+    callBack();
 
 }
 
+
+
 //easterlywave에서 열대성 저기압 리스트를 가져온다.
-const parseTyphoonListFromEasterlyWave = ($:cheerio.CheerioAPI) => {
+const parseTyphoonListFromEasterlyWave:parsetHtmlDetailInterface = ($) => {
 
     const lists = $(".level");
     lists.each((index, list) => {
         //crawling from easterlywave 
     });
+
+
+    return new Array<TyphoonInfo>; //임시로 넣어뒀음. 나중에 파싱 추가하면 넣어야한다.
 }
+
+const parseList = {
+    parseTyphoonListFromEasterlyWave,
+    
+};
+
